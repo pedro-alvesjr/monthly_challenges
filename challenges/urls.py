bbbee6e78bfc453461de9bfec0b1ea109554e217
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-10-14 00:39:35 -0300

    Update urls.py

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path[""]
+    path("")
     path('<int:month>', views.monthly_challenge_by_number),
     path('<str:month>', views.monthly_challenge, name='month-challenge')
 ]
