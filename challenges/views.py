62e140d276136d02deed2ec1d76b631577fcda8f
Pedro Alves <pedromoraes.junior96@gmail.com>
2025-10-19 14:28:51 -0300

    Update views.py

--- challenges/views.py
+++ challenges/views.py
@@ -48,7 +48,7 @@ def monthly_challenge(request, month):
         challenge_text = monthly_challenges[month]
         return render(request, 'challenges/challenge.html', {
             'text': challenge_text,
-            'month_name': month.capitalize()
+            'month_name': month
             })
     except:
         HttpResponseNotFound('<h1>Invalid month.</h1>')
